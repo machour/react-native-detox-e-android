e5375714d03a53655cfaea70f608f1d9ee86e73e
Marco Blumendorf <marco@iamsmart.com>
2018-02-22 11:57:01 +0100

    tests are running

--- App.js
+++ App.js
@@ -4,54 +4,48 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, { Component } from 'react';
+ import {
+   AppRegistry,
+   StyleSheet,
+   Text,
+   View,
+   TouchableOpacity
+ } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-export default class App extends Component<{}> {
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        greeting: undefined
+      };
+    }
   render() {
+    if (this.state.greeting) return this.renderAfterButton();
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View testID='welcome' style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 25, marginBottom: 30}}>
+          Welcome
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <TouchableOpacity testID='hello_button' onPress={this.onButtonPress.bind(this, 'Hello')}>
+          <Text style={{color: 'blue', marginBottom: 20}}>Say Hello</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  renderAfterButton() {
+    return (
+      <View style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 25}}>
+          {this.state.greeting}!!!
         </Text>
       </View>
     );
   }
+  onButtonPress(greeting) {
+    this.setState({
+      greeting: greeting
+    });
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- __tests__/App.js
+++ __tests__/App.js
@@ -5,8 +5,16 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
+describe('Integration Test: Detox via Jest as test runner (Not mocha)', () => {
+
+it('should have welcome screen', async () => {
+  await expect(element(by.id('welcome'))).toBeVisible();
 });
+
+it('should show hello screen after tap', async () => {
+  await element(by.id('hello_button')).tap();
+  await expect(element(by.text('Hello!!!'))).toBeVisible()
+});
+
+
+})
